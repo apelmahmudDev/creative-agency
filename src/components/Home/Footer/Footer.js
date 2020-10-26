e19b76c0c6d4dc0673a213fa62054e90566e3c1f
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-26 18:02:24 +0800

    Home page is completely done

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -4,36 +4,35 @@ const Footer = () => {
     const styles = {
         footerArea: {
             backgroundColor: '#FBD062',
+            padding: '5% 5% 2% 5%'
         }
     }
     return (
-        <Footer style={styles.footerArea}>
+        <footer style={styles.footerArea}>
             <div className="container">
                 <div className="row">
                     <div className="col-md-5">
-                        <h2 className="text-brand">Let us handle your <br/> project, professionally.</h2>
+                        <h2 className="text-brand mb-5">Let us handle your <br/> project, professionally.</h2>
                         <p>With well written codes, we build amazing apps for all platforms, mobile and web apps in general.</p>
                     </div>
                     <div className="col-md-7">
                     <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <div className="form-group">
+                            <input type="email" className="form-control" placeholder="Your email address" />
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Your name / company's name" />
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <textarea class="form-control"/>
+                        <div className="form-group">
+                            <textarea className="form-control" rows="8" placeholder="Your message"/>
                         </div>
-                        <button type="submit" class="btn btn-brand">Submit</button>
+                        <button type="submit" className="btn btn-brand">Submit</button>
                     </form>
                     </div>
                 </div>
+                <p className="text-center mt-5 pt-5">&copy;-copyright mahmud {new Date().getFullYear()}</p>
             </div>
-        </Footer>
+        </footer>
     );
 };
 
